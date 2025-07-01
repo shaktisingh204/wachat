3efc63d3116979ea796a5a9b67d3c3bf5f964cc7
Shakti Singh <ratansingh111786a@gmail.com>
2025-07-01 17:50:25 +0000

    Failed to load resource: the server responded with a status of 413 (Requ

---
+++ src/next.config.ts
@@ -0,0 +1,35 @@
+
+const withBundleAnalyzer = require('@next/bundle-analyzer')({
+  enabled: process.env.ANALYZE === 'true',
+});
+
+const nextConfig = {
+  /* config options here */
+  serverActions: {
+    bodySizeLimit: '50mb',
+  },
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'placehold.co',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'wawachat.s3.us-east-1.amazonaws.com',
+        port: '',
+        pathname: '/**',
+      },
+    ],
+  },
+};
+
+module.exports = withBundleAnalyzer(nextConfig);
