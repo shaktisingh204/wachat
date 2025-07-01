425f666b6db62f49a08f8565c14fa1034865f65d
Shakti Singh <ratansingh111786a@gmail.com>
2025-07-01 16:40:17 +0000

    Error (#100) Param categories[0] must be one of {SIGN UP, SIGN IN, APPOI

--- src/components/wabasimplify/meta-flow-templates.ts
+++ src/components/wabasimplify/meta-flow-templates.ts
@@ -1,22 +1,31 @@
 
+
 export const flowCategories = [
     { id: 'LEAD_GENERATION', name: 'Lead Generation' },
     { id: 'CUSTOMER_SUPPORT', name: 'Customer Support' },
     { id: 'APPOINTMENT_BOOKING', name: 'Appointment Booking' },
-    { id: 'PRODUCT_RECOMMENDATION', name: 'Product Recommendation' },
-    { id: 'ORDER_TRACKING', name: 'Order Tracking' },
-    { id: 'ONBOARDING', name: 'Onboarding' },
-    { id: 'FEEDBACK_COLLECTION', name: 'Feedback Collection' },
-    { id: 'APPLICATION_PROCESS', name: 'Application Process' },
-    { id: 'SUBSCRIPTION_MANAGEMENT', name: 'Subscription Management' },
     { id: 'SURVEY', name: 'Survey' },
     { id: 'SIGN_UP', name: 'Sign Up' },
     { id: 'SIGN_IN', name: 'Sign In' },
-    { id: 'CUSTOM', name: 'Custom' },
+    { id: 'CONTACT_US', name: 'Contact Us' },
+    { id: 'SHOPPING', name: 'Shopping' },
     { id: 'OTHER', name: 'Other' },
 ];
 
-export const uiComponents = [
+export type UIComponent = {
+    id?: string;
+    type: 'TextHeading' | 'TextBody' | 'TextSubtext' | 'Image' | 'TextInput' | 'DatePicker' | 'RadioButtons' | 'CheckboxGroup' | 'Dropdown' | 'OptIn';
+    name?: string;
+    label?: string;
+    text?: string;
+    url?: string;
+    caption?: string;
+    'input-type'?: 'text' | 'number' | 'email';
+    'data-source'?: { id: string, title: string }[];
+};
+
+
+export const uiComponents: { type: UIComponent['type'], label: string }[] = [
     { type: 'TextHeading', label: 'Heading Text' },
     { type: 'TextBody', label: 'Body Text' },
     { type: 'TextSubtext', label: 'Sub-text' },
