9801f79e7490c9eba9316e1a9dd81937eff601d5
Shakti Singh <ratansingh111786a@gmail.com>
2025-07-02 04:18:18 +0000

    remove this generate-meta-flow.ts completly i don't need ai in this

--- src/ai/dev.ts
+++ src/ai/dev.ts
@@ -6,6 +6,5 @@ import '@/ai/flows/template-content-suggestions.ts';
 import '@/ai/flows/translate-text.ts';
 import '@/ai/flows/auto-reply-flow.ts';
 import '@/ai/flows/intelligent-translate-flow.ts';
-import '@/ai/flows/generate-meta-flow.ts';
 import '@/ai/flows/generate-flow-builder-flow.ts';
 
