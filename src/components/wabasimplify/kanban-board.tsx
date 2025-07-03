af2a25be926661bd3d5d008c885dc3585943eb46
Shakti Singh <ratansingh111786a@gmail.com>
2025-07-03 14:27:27 +0000

    add this height:100% on
    <div style="min-width: 100%;display: table; he

--- src/components/wabasimplify/kanban-board.tsx
+++ src/components/wabasimplify/kanban-board.tsx
@@ -162,11 +162,13 @@ export function KanbanBoard() {
     
     return (
         <ScrollArea className="w-full h-full">
-            <div className="flex h-full w-max p-4 gap-4">
-                {boardData.map(column => (
-                    <KanbanColumn key={column.name} title={column.name} contacts={column.contacts} onDrop={handleDrop} />
-                ))}
-                 <AddList onAddList={handleAddList} />
+            <div style={{minWidth: '100%', display: 'table', height: '100%'}}>
+                <div className="flex h-full w-max p-4 gap-4">
+                    {boardData.map(column => (
+                        <KanbanColumn key={column.name} title={column.name} contacts={column.contacts} onDrop={handleDrop} />
+                    ))}
+                    <AddList onAddList={handleAddList} />
+                </div>
             </div>
             <ScrollBar orientation="horizontal" />
         </ScrollArea>
