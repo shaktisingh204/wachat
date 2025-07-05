c1eca4356cb2d18c2849fc177fa9cb218e38209c
Shakti Singh <ratansingh111786a@gmail.com>
2025-07-05 18:39:42 +0000

    ./node_modules/@opentelemetry/instrumentation/build/esm/platform/node/in

--- instrumentation.ts
+++
@@ -1,6 +0,0 @@
-
-export async function register() {
-  // This file can be used to register instrumentation for your app.
-  // For example, you can register OpenTelemetry instrumentation here.
-  // See https://nextjs.org/docs/app/building-your-application/optimizing/instrumentation
-}
