e207bad1e0821a783a356e56956d1f502c281707
Shakti Singh <ratansingh111786a@gmail.com>
2025-07-08 01:43:34 +0000

    add all

--- src/components/wabasimplify/website-builder/website-block-editor.tsx
+++ src/components/wabasimplify/website-builder/website-block-editor.tsx
@@ -6,7 +6,6 @@ import { HeroBlockEditor } from './hero-block-editor';
 import { FeaturedProductsBlockEditor } from './featured-products-block-editor';
 import { RichTextBlockEditor } from './rich-text-block-editor';
 import { TestimonialsBlockEditor } from './testimonials-block-editor';
-import { FaqBlockRenderer } from './faq-block-renderer';
 import { CustomHtmlBlockEditor } from './custom-html-block-editor';
 import { HeadingBlockEditor } from './heading-block-editor';
 import { ImageBlockEditor } from './image-block-editor';
@@ -20,16 +19,14 @@ import { AccordionBlockEditor } from './accordion-block-editor';
 import { FormBlockEditor } from './form-block-editor';
 import { MapBlockEditor } from './map-block-editor';
 import { CountdownBlockEditor } from './countdown-block-editor';
-import { SocialShareBlockRenderer } from './social-share-block-renderer';
-import { RepeaterBlockRenderer } from './repeater-block-renderer';
+import { SocialShareBlockEditor } from './social-share-block-editor';
+import { RepeaterBlockEditor } from './repeater-block-editor';
 import { SectionBlockEditor } from './section-block-editor';
 import { ColumnsBlockEditor } from './columns-block-editor';
 import { ColumnBlockEditor } from './column-block-editor';
 import { Card, CardContent, CardDescription, CardHeader, CardTitle, CardFooter } from '@/components/ui/card';
 import { Button } from '@/components/ui/button';
 import { Trash2 } from 'lucide-react';
-import { SocialShareBlockEditor } from './social-share-block-editor';
-import { RepeaterBlockEditor } from './repeater-block-editor';
 import { FaqBlockEditor } from './faq-block-editor';
 
 
