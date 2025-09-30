4949a276ff3130c9f35798c58e0b542a7756cc35
Shakti Singh <ratansingh111786a@gmail.com>
2025-09-30 05:54:26 +0000

    Build error occurred [Error:   × Unexpected token `type`. Expected ... ,

--- next.config.ts
+++ next.config.ts
@@ -1,16 +1,11 @@
-<<<<<<< HEAD
+
+import type { NextConfig } from 'next';
 
 const withBundleAnalyzer = require('@next/bundle-analyzer')({
   enabled: process.env.ANALYZE === 'true',
 });
 
-const nextConfig = {
-=======
-import type {NextConfig} from 'next';
-
 const nextConfig: NextConfig = {
-  /* config options here */
->>>>>>> 253f92ef (Initialized workspace with Firebase Studio)
   typescript: {
     ignoreBuildErrors: true,
   },
@@ -27,17 +22,18 @@ const nextConfig: NextConfig = {
       },
       {
         protocol: 'https',
-<<<<<<< HEAD
         hostname: 'wawachat.s3.us-east-1.amazonaws.com',
-=======
+        port: '',
+        pathname: '/**',
+      },
+       {
+        protocol: 'https',
         hostname: 'images.unsplash.com',
->>>>>>> 253f92ef (Initialized workspace with Firebase Studio)
         port: '',
         pathname: '/**',
       },
       {
         protocol: 'https',
-<<<<<<< HEAD
         hostname: 'scontent.whatsapp.net',
         port: '',
         pathname: '/**',
@@ -57,15 +53,17 @@ const nextConfig: NextConfig = {
       {
         protocol: 'https',
         hostname: 'static5.lenskart.com',
-=======
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
         hostname: 'picsum.photos',
->>>>>>> 253f92ef (Initialized workspace with Firebase Studio)
         port: '',
         pathname: '/**',
       },
     ],
   },
-<<<<<<< HEAD
   webpack: (config, { isServer }) => {
     if (!isServer) {
       // Exclude server-only modules from the client-side build
@@ -85,8 +83,3 @@ const nextConfig: NextConfig = {
 };
 
 module.exports = withBundleAnalyzer(nextConfig);
-=======
-};
-
-export default nextConfig;
->>>>>>> 253f92ef (Initialized workspace with Firebase Studio)
