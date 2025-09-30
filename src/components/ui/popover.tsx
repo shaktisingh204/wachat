30d8787d0ed5aac171fea54397852ed2f4c99724
Shakti Singh <ratansingh111786a@gmail.com>
2025-09-30 06:22:41 +0000

    ./src/components/ui/popover.tsx Module parse failed: Duplicate export 'P

--- src/components/ui/popover.tsx
+++ src/components/ui/popover.tsx
@@ -1,3 +1,4 @@
+
 "use client"
 
 import * as React from "react"
@@ -31,4 +32,3 @@ const PopoverContent = React.forwardRef<
 PopoverContent.displayName = PopoverPrimitive.Content.displayName
 
 export { Popover, PopoverTrigger, PopoverContent, PopoverAnchor }
-export { Popover, PopoverTrigger, PopoverContent }
