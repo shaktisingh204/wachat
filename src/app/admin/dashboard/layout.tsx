cf3c0280c58377e9e3f5062cf629564cc958ec93
Shakti Singh <ratansingh111786a@gmail.com>
2025-10-06 18:59:31 +0000

    on dashboard also add list of recent boradcasts from across all users

--- src/app/admin/dashboard/layout.tsx
+++ src/app/admin/dashboard/layout.tsx
@@ -109,7 +109,7 @@ export default function AdminDashboardLayout({ children }: { children: React.Rea
             </DropdownMenu>
           </div>
         </header>
-        <div className="flex flex-1 min-h-0">
+        <div className="flex-1 min-h-0">
           <main className="flex-1 p-2 md:p-4 overflow-y-auto">{children}</main>
         </div>
       </SidebarInset>
