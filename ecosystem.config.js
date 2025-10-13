8b589703fca9513b415c9ec76ef50523fcb9d09d
Shakti Singh <ratansingh111786a@gmail.com>
2025-10-13 08:03:36 +0000

    [13:30:20] Broadcast moved to QUEUED state with 6661 contacts ready. [13

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,28 +2,12 @@
 module.exports = {
   apps: [
     {
-      name: 'sabnode-web',
-      script: 'server.js',
-      instances: 'max',
-      exec_mode: 'cluster',
-      env: {
-        NODE_ENV: 'production',
-        PORT: 3001,
-      },
-      // Ensure we don't try to run workers inside the web server instances
-      env_production: {
-        IS_WORKER: 'false',
-      },
-    },
-    {
       name: 'sabnode-worker',
       script: 'worker.js',
-      instances: 'max', // Use all available CPUs minus one for the web server
+      instances: 'max', // Use all available CPUs for workers
       exec_mode: 'cluster',
       env: {
         NODE_ENV: 'production',
-        // This flag tells the script it's a worker
-        IS_WORKER: 'true',
       },
     },
   ],
