ff6983297f92e15cf0d6155458b938115d46f370
Shakti Singh <ratansingh111786a@gmail.com>
2025-10-14 07:22:17 +0000

    sabnode-web       │ default     │ 0.1.0   │ fork    │ 0        │ 0

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,6 +2,16 @@
 module.exports = {
   apps: [
     {
+      name: 'sabnode-web',
+      script: 'node_modules/next/dist/bin/next',
+      args: 'start -p 3001',
+      instances: 'max', // Or a specific number, e.g., 2
+      exec_mode: 'cluster',
+      env: {
+        NODE_ENV: 'production',
+      },
+    },
+    {
       name: 'sabnode-worker',
       script: 'worker.js',
       instances: 'max', // Use all available CPUs for workers
