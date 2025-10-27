683b391b592bad27fdea0424d16bf31d056cb764
Shakti Singh <ratansingh111786a@gmail.com>
2025-10-27 16:42:00 +0000

    remove from all these pages Account Groups Chart of Accounts Voucher Boo

--- src/app/dashboard/crm/accounting/vouchers/[voucherBookId]/page.tsx
+++ src/app/dashboard/crm/accounting/vouchers/[voucherBookId]/page.tsx
@@ -1,7 +1,7 @@
 
 'use client';
 
-import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card";
+import { Card, CardContent, CardDescription, CardHeader, CardTitle, CardFooter } from "@/components/ui/card";
 import { Table, TableBody, TableCell, TableHead, TableHeader, TableRow } from "@/components/ui/table";
 import { Button } from "@/components/ui/button";
 import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from '@/components/ui/select';
