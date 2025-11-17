baa48c2fb1ef9a05d4c977771b0c7896427ccf85
Shakti Singh <ratansingh111786a@gmail.com>
2025-11-17 08:18:16 +0000

    Disable Genkit server during production
    In your Genkit entry file (usua

--- src/ai/dev.ts
+++ src/ai/dev.ts
@@ -1,5 +1,9 @@
 import { config } from 'dotenv';
 config();
 
+if (process.env.NODE_ENV === "production") {
+  process.env.GENKIT_DISABLE_SERVER = "1";
+}
+
 import '@/ai/flows/generate-post-suggestions.ts';
 import '@/ai/flows/generate-promo-video.ts';
