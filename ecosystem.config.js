e7f8e5206317e95fd9c020b1cc6a54377d2bc695
Shakti Singh <ratansingh111786a@gmail.com>
2025-11-17 11:36:09 +0000

    keep only one sabnode-worker-high    │ default     │ 0.1.0   │ cluster │

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -10,29 +10,14 @@ module.exports = {
       env: { NODE_ENV: 'production' },
     },
     {
-      name: 'sabnode-worker-low',
+      name: 'sabnode-worker',
       script: './worker.js',
-      args: 'low-priority-broadcasts',
       instances: 1,
       exec_mode: 'cluster',
       restart_delay: 10000, // 10s delay to reduce rapid restarts
       max_restarts: 10,     // allow more retries
       env: {
         NODE_ENV: 'production',
-        KAFKA_TOPIC: 'low-priority-broadcasts',
-      },
-    },
-    {
-      name: 'sabnode-worker-high',
-      script: './worker.js',
-      args: 'high-priority-broadcasts',
-      instances: 1, // reduce to 1 for stability
-      exec_mode: 'cluster',
-      restart_delay: 10000, // 10s delay
-      max_restarts: 10,
-      env: {
-        NODE_ENV: 'production',
-        KAFKA_TOPIC: 'high-priority-broadcasts',
       },
     },
   ],
