779c442cec8291e6a23378089fd8f6329ddf107b
shaktisingh204 <54027298+shaktisingh204@users.noreply.github.com>
2025-11-18 00:11:29 +0530

    Update cron-scheduler.js

--- src/lib/cron-scheduler.js
+++ src/lib/cron-scheduler.js
@@ -2,10 +2,10 @@
 'use strict';
 
 require('dotenv').config();
-const { connectToDatabase } = require('../mongodb.js');
+const { connectToDatabase } = require('./mongodb.js');
 const { Kafka, Partitioners } = require('kafkajs');
 const { ObjectId } = require('mongodb');
-const { getErrorMessage } = require('../utils.js');
+const { getErrorMessage } = require('./utils.js');
 
 const KAFKA_BROKERS = process.env.KAFKA_BROKERS?.split(',') || ['127.0.0.1:9092'];
 const KAFKA_TOPIC = 'broadcasts';
