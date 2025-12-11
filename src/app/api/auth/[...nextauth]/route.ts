e702b4415e67e26f197fb10c093caf14ad74e4b7
Shakti Singh <ratansingh111786a@gmail.com>
2025-12-11 09:36:39 +0000

    Login Failed (0 , next_headers__WEBPACK_IMPORTED_MODULE_2__.cookies)(...

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -2,12 +2,16 @@
 import NextAuth from 'next-auth';
 import Google from 'next-auth/providers/google';
 import Facebook from 'next-auth/providers/facebook';
+import Credentials from 'next-auth/providers/credentials';
 import { connectToDatabase } from '@/lib/mongodb';
 import { MongoDBAdapter } from '@auth/mongodb-adapter';
 import type { Adapter } from 'next-auth/adapters';
+import { comparePassword } from '@/lib/auth';
+import type { User } from '@/lib/definitions';
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
     adapter: MongoDBAdapter(connectToDatabase().then(c => c.client), { databaseName: process.env.MONGODB_DB }) as Adapter,
+    session: { strategy: 'jwt' },
     providers: [
       Google({
         clientId: process.env.GOOGLE_CLIENT_ID,
@@ -17,6 +21,26 @@ export const { handlers, signIn, signOut, auth } = NextAuth({
         clientId: process.env.FACEBOOK_CLIENT_ID,
         clientSecret: process.env.FACEBOOK_CLIENT_SECRET,
       }),
+      Credentials({
+          async authorize(credentials) {
+            if (!credentials?.email || !credentials.password) {
+                return null;
+            }
+            const { db } = await connectToDatabase();
+            const user = await db.collection<User>('users').findOne({ email: (credentials.email as string).toLowerCase() });
+
+            if (!user || !user.password) {
+                return null;
+            }
+
+            const passwordMatch = await comparePassword(credentials.password as string, user.password);
+            if (!passwordMatch) {
+                return null;
+            }
+
+            return { id: user._id.toString(), name: user.name, email: user.email, image: user.image };
+          }
+      })
     ],
     callbacks: {
         async signIn({ user, account, profile }) {
@@ -44,14 +68,20 @@ export const { handlers, signIn, signOut, auth } = NextAuth({
             }
             return true;
         },
-        async session({ session, user }) {
+        async session({ session, token }) {
             const { db } = await connectToDatabase();
-            const dbUser = await db.collection('users').findOne({ _id: new ObjectId(user.id) });
+            const dbUser = await db.collection('users').findOne({ email: token.email! }, { projection: { password: 0 }});
 
             if (dbUser) {
-                session.user = { ...session.user, ...dbUser };
+                session.user = { ...session.user, ...dbUser, _id: dbUser._id.toString() };
             }
             return session;
+        },
+        async jwt({ token, user }) {
+            if (user) {
+                token.id = user.id;
+            }
+            return token;
         }
     },
     pages: {
