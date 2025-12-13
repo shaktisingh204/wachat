fdec6208697fbb78e2da54acd32ec15d9f31f0ed
Shakti Singh <ratansingh111786a@gmail.com>
2025-12-13 07:24:51 +0000

    verifying Firebase ID token in server component: Error: Failed to parse

--- src/lib/firebase/service-account.ts
+++ src/lib/firebase/service-account.ts
@@ -4,7 +4,7 @@ export const serviceAccount = {
   "type": "service_account",
   "project_id": "your-project-id", // <-- REPLACE with your project ID
   "private_key_id": "your-private-key-id", // <-- REPLACE
-  "private_key": "-----BEGIN PRIVATE KEY-----\nYOUR_PRIVATE_KEY\n-----END PRIVATE KEY-----\n", // <-- REPLACE
+  "private_key": `-----BEGIN PRIVATE KEY-----\nYOUR_PRIVATE_KEY\n-----END PRIVATE KEY-----\n`, // <-- REPLACE
   "client_email": "your-client-email", // <-- REPLACE
   "client_id": "your-client-id", // <-- REPLACE
   "auth_uri": "https://accounts.google.com/o/oauth2/auth",
