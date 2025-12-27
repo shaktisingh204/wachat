f89677f204aa3cd1a0856c666d606ad41c10a0cd
root <root@e2e-103-130.ssdcloudindia.net>
2025-12-27 13:47:17 +0530

    keep server changes after rebase

--- src/lib/firebase/service-account.ts
+++ src/lib/firebase/service-account.ts
@@ -1,14 +1,16 @@
 // This file is intended for server-side use only.
 // Do not import this file in any client-side code.
 export const serviceAccount = {
-  "type": "service_account",
-  "project_id": "your-project-id", // <-- REPLACE with your project ID
-  "private_key_id": "your-private-key-id", // <-- REPLACE
-  "private_key": `-----BEGIN PRIVATE KEY-----\nYOUR_PRIVATE_KEY\n-----END PRIVATE KEY-----\n`, // <-- REPLACE
-  "client_email": "your-client-email", // <-- REPLACE
-  "client_id": "your-client-id", // <-- REPLACE
-  "auth_uri": "https://accounts.google.com/o/oauth2/auth",
-  "token_uri": "https://oauth2.googleapis.com/token",
-  "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
-  "client_x509_cert_url": "your-client-x509-cert-url" // <-- REPLACE
-};
+    "type": "service_account",
+    "project_id": "sabnode-408f8",
+    "private_key_id": "b4153a9d1d1acd63d3a4868657ca6820d88cecea",
+    "private_key": '-----BEGIN PRIVATE KEY-----\nMIIEvQIBADANBgkqhkiG9w0BAQEFAASCBKcwggSjAgEAAoIBAQCqBHHtMu8z/gVr\nOHjh8tC+/eRh3VI4P2O2mp/W/D2D7lOdy1YKMfC01LBrXKhLdBtRNZsAHpGQPY3b\nlrGVf9ewHMbduFfa3plyDAte6eBJnaZVXH8CyHPF6P4hp3xuorMwjTp/djNekzCV\nJheBHeXqfJ4z3qULU7YRUc98O7Q7nLk/iHG3nxKdn2KyL2XJHb1FcWF7vMDKDn8m\nmLdd7sD3SaCFUmBeyBqFICWSvdGH5xX/II5HI/8PRO81Y0x/byUS7fHpaNSY01wd\nkBJAb982nebGJzWeRdP6nNVBxWmdwvM4kG2XXr0xsu70754MFyXJtVTGhnq/Clf3\nCyt6VOE9AgMBAAECggEAOZEovC6PZPThg9UKxULQ4KKaJnLoiIgaLLW9Ym9lKttT\nL64uU2RmguwzUh79ExNcvooekhNHqWXWNyCWv66aaxTLVz64bLELltyyKvCY04RQ\nUj3v+ZLC9KO4DNAnp1RqxQlE4ey1ZxQCkrXjpoHnnBaXAKwmCShOkR242fFjTP1t\nMzejn+hcjPN9fIbTy2g5UnNqunnngxHGyijHkjt5hdvVIrQyvwshx+cYlioJuVob\nDxXu98gF943lTU1QOP+cWZaU/ACoo6jFvGQl+V7mvTmjS+dBtBNyFGi1FtOQSj4t\nloqwqXFrn+Z8F9k9mtKMLgC8Ll7wdqkHD7DYo/S4pwKBgQDsMxAWLbBQkgWL9RPz\n7PUNcRyNTKKM15MkJGhkghq0/ckMahjGBqt8Lc05ldON6wL0zF0yrsl66/vS/KCj\n5oduQIXzzi1qvTEgV/KIyc66IN6Ego2QV+NJYQbPzGwJ0+IYukxImzYMWhp0Qf/0\nQc9kY0JQD+GRiS7bRyvoXqCNbwKBgQC4RRZUCKY9f80xsqJdprLfr7qqOiKtNRKi\nhKF87SHqPPajFgYjqPR6ukYMGMHFu+Q+/qodibm2VQVYwFRx1H+pqA/5anOMXxNT\nohuCz/mfXrwGx4ZcdWxUC7CeYTFx8wAdDl1xDJX/f/gX902fXztNRItqebUI/Ibp\nIJWgdq6+EwKBgEwMoNhTO5qleU23ygXWvIvoaGYCT7ADVOflJQt/bHPr5oitvG4+\nBYbFBHfPCr999IS3LCa+aja+tmUXW+2Jsv9HnRmDv9HHOoMMbhWmzH3XSW3mzwez\nlIWyzMupKPv9EaYejaMu/jb559yYx31wlwLehqJaiqkeP0zw3G1D0nPXAoGBAJdm\ntGTRQn2vPxJBWqSl7bxab32kDI/R1oDPhrvVPpNuCZuP4fV0wYMA/8K6qKNlsLCN\nn4prebo0+xKj2/43AbmMy6s7arr/9yo1PHNrnAb02Hqdf2kCVcJsZG6NHwooBYTa\nI65aD/iA1YAjgpSas2fWFNKkMzmx+ssbKbVRLlwfAoGAThJXNnm9/0HbOirBtu1B\nzjweaCCdAiF6lBUuGXGEzPgJk2lbILhmsc1PKArZWr3vAdetvSJH1qr98CRPwrzS\ngziAq4k2RCH64yLNPAWN8Id38jWPYXYEUlBfBGw4f5/fKFN1Us4+Tf6lWBE53KQk\ncP1LfRiMRN/I5mkabs3Y90Y=\n-----END PRIVATE KEY-----\n',
+    "client_email": "firebase-adminsdk-ws5j0@sabnode-408f8.iam.gserviceaccount.com",
+    "client_id": "108923413398884916881",
+    "auth_uri": "https://accounts.google.com/o/oauth2/auth",
+    "token_uri": "https://oauth2.googleapis.com/token",
+    "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
+    "client_x509_cert_url": "https://www.googleapis.com/robot/v1/metadata/x509/firebase-adminsdk-ws5j0%40sabnode-408f8.iam.gserviceaccount.com",
+    "universe_domain": "googleapis.com"
+  };
+
