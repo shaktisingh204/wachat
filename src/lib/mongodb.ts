63900f7cb87d1ea0219a23b820b379edbd5d3e3d
Shakti Singh <ratansingh111786a@gmail.com>
2026-01-12 10:29:46 +0000

    Login Failed An unexpected server error occurred.
    on admin login

--- src/lib/mongodb.ts
+++ src/lib/mongodb.ts
@@ -1,5 +1,8 @@
-
 import { MongoClient, Db } from 'mongodb';
+
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config({ path: require('path').resolve(process.cwd(), '.env') });
+}
 
 if (!process.env.MONGODB_URI) {
   throw new Error('Please define the MONGODB_URI environment variable inside .env.local');
